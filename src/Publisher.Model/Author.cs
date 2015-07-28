74e159d25531473907c2d8b9568305e9adf11504
Robin Kanters <robin@robinkanters.nl>
2015-07-28 21:52:52 +0200

    Added models for Story and Author

---
+++ src/Publisher.Model/Author.cs
@@ -0,0 +1,10 @@
+﻿using Publisher.ModelInterfaces;
+
+namespace Publisher.Model
+{
+    class Author : IAuthor
+    {
+        public string Name { get; set; }
+        public string Biography { get; set; }
+    }
+}
---
+++ src/Publisher.Model/Story.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Publisher.ModelInterfaces;
+
+namespace Publisher.Model
+{
+    public class Story : IStory, ITaggable
+    {
+        public void Tag(IPostTag tag)
+        {
+            Tags.Add(tag);
+        }
+
+        public List<IPostTag> Tags { get; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime PublishDate { get; set; }
+        public IAuthor Author { get; set; }
+        public string Description { get; set; }
+    }
+}
